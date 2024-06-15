11a409edfd734f14df779fd6e5dd760c1c216715
YurenaLopez <7836067@alumnos.ufv.es>
2024-06-15 14:21:02 +0200

    se añade el main

--- Backend/src/main/java/ufv/_4/examenprueba/yls/ProductosApplication.java
+++ Backend/src/main/java/ufv/_4/examenprueba/yls/ProductosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YlsApplication {
+public class ProductosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YlsApplication.class, args);
+		SpringApplication.run(ProductosApplication.class, args);
 	}
 
 }
