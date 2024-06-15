a7396892f894bcd662e27e4509cb5bbea7f41ea7
YurenaLopez <7836067@alumnos.ufv.es>
2024-06-15 14:05:06 +0200

    first commit

---
+++ Backend/src/main/java/ufv/_4/examenprueba/yls/YlsApplication.java
@@ -0,0 +1,13 @@
+package ufv._4.examenprueba.yls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YlsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/ufv/_4/examenprueba/yls/YlsApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv._4.examenprueba.yls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
